8d2218e9e81064a70b4252fe44de5894f0515b61
Gershwen McPherson <gershwen@3stepweb.co.uk>
2025-02-08 19:14:39 +0200

    updated domain

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,82 +1,174 @@
-import data from "../../data.json" assert { type: "json" };
-import blogs from "../../blogs.json" assert {type: "json"};
+// import data from "../../data.json" assert { type: "json" };
+// import blogs from "../../blogs.json" assert {type: "json"};
 
-let section = document.querySelector(".posts");
+// let section = document.querySelector(".posts");
 
-let i = 0;
+// let i = 0;
 
-data.forEach((project) => {
-  let article = document.createElement("article");
+// data.forEach((project) => {
+//   let article = document.createElement("article");
 
-  article.innerHTML = `           <header>
-    <h2><a href=${project.appUri}>${project.name}<br />
-        </a></h2>
-</header>
-<a href=${project.appUri} class="image fit"><img
-        src=${project.imgsrc} alt=${project.alt} /></a>
-<p>${project.description}</p>
-<ul class="actions special">
-    <li><a href=${project.appUri} class="button">Check it
-            out</a></li>
-</ul>
-<a href=${project.githubrepo}>Click here to view source code</a>
-`;
+//   article.innerHTML = `           <header>
+//     <h2><a href=${project.appUri}>${project.name}<br />
+//         </a></h2>
+// </header>
+// <a href=${project.appUri} class="image fit"><img
+//         src=${project.imgsrc} alt=${project.alt} /></a>
+// <p>${project.description}</p>
+// <ul class="actions special">
+//     <li><a href=${project.appUri} class="button">Check it
+//             out</a></li>
+// </ul>
+// <a href=${project.githubrepo}>Click here to view source code</a>
+// `;
 
-  article.value = i;
-  i = i + 1;
-  section.appendChild(article);
-});
+//   article.value = i;
+//   i = i + 1;
+//   section.appendChild(article);
+// });
 
-//Skills
+// //Skills
 
-const skills = [
-  "Semantic HTML",
-  "CSS",
-  "JavaScript",
-  "jQuery",
-  "Bootstrap",
-  "React.js",
-  "Next.js",
-  "Node.js",
-  "Express",
-  "Mongoose",
-  "MongoDB",
-  "Authentication with JWT",
-  "Git & GitHub",
-  "Writing Markdown",
-  "Experience using Figma design files",
-  "Excellent communicator",
-  "Leadership",
-];
+// const skills = [
+//   "GraphQl with Apollo",
+//   "Semantic HTML",
+//   "CSS",
+//   "JavaScript",
+//   "jQuery",
+//   "Bootstrap",
+//   "React.js",
+//   "Next.js",
+//   "Node.js",
+//   "Express",
+//   "Mongoose",
+//   "MongoDB",
+//   "Authentication with JWT",
+//   "Git & GitHub",
+//   "Writing Markdown",
+//   "Experience using Figma design files",
+//   "Excellent communicator",
+//   "Leadership",
+// ];
 
-let skillsList = document.querySelector("#skills-list");
+// let skillsList = document.querySelector("#skills-list");
 
-let j = 0;
+// let j = 0;
 
-skills.forEach((skill) => {
-  let listItem = document.createElement("li");
+// skills.forEach((skill) => {
+//   let listItem = document.createElement("li");
 
-  listItem.innerHTML = skill;
+//   listItem.innerHTML = skill;
 
-  skillsList.value = j;
-  j = j + 1;
-  skillsList.appendChild(listItem);
-});
+//   skillsList.value = j;
+//   j = j + 1;
+//   skillsList.appendChild(listItem);
+// });
 
 
-/////////////Blogs////////////////////
+// /////////////Blogs////////////////////
 
-let blogsList = document.querySelector("#blog-list");
+// let blogsList = document.querySelector("#blog-list");
 
-let g = 0;
+// let g = 0;
 
-blogs.forEach((blog) => {
+// blogs.forEach((blog) => {
 
-  let blogItem = document.createElement("li");
+//   let blogItem = document.createElement("li");
 
-  blogItem.innerHTML = `<a href=${blog.blogurl}>${blog.blogname}</a>`;
+//   blogItem.innerHTML = `<a href=${blog.blogurl}>${blog.blogname}</a>`;
 
-blogItem.value = g;
-  g = g + 1;
-  blogsList.appendChild(blogItem);
-});
+// blogItem.value = g;
+//   g = g + 1;
+//   blogsList.appendChild(blogItem);
+// });
+
+async function fetchData() {
+  try {
+    // Fetch projects data
+    const dataResponse = await fetch("../../data.json");
+    const data = await dataResponse.json();
+
+    // Fetch blogs data
+    const blogsResponse = await fetch("../../blogs.json");
+    const blogs = await blogsResponse.json();
+
+    // Render content
+    renderProjects(data);
+    renderSkills();
+    renderBlogs(blogs);
+  } catch (error) {
+    console.error("Error loading JSON files:", error);
+  }
+}
+
+fetchData();
+
+// Function to render projects
+function renderProjects(data) {
+  let section = document.querySelector(".posts");
+  if (!section) return;
+
+  data.forEach((project) => {
+    let article = document.createElement("article");
+
+    article.innerHTML = `
+      <header>
+        <h2><a href="${project.appUri}">${project.name}<br /></a></h2>
+      </header>
+      <a href="${project.appUri}" class="image fit">
+        <img src="${project.imgsrc}" alt="${project.alt}" />
+      </a>
+      <p>${project.description}</p>
+      <ul class="actions special">
+        <li><a href="${project.appUri}" class="button">Check it out</a></li>
+      </ul>
+      <a href="${project.githubrepo}">Click here to view source code</a>
+    `;
+    section.appendChild(article);
+  });
+}
+
+// Function to render skills
+function renderSkills() {
+  const skills = [
+    "GraphQl with Apollo",
+    "Semantic HTML",
+    "CSS",
+    "JavaScript",
+    "jQuery",
+    "Bootstrap",
+    "React.js",
+    "Next.js",
+    "Node.js",
+    "Express",
+    "Mongoose",
+    "MongoDB",
+    "Authentication with JWT",
+    "Git & GitHub",
+    "Writing Markdown",
+    "Experience using Figma design files",
+    "Excellent communicator",
+    "Leadership",
+  ];
+
+  let skillsList = document.querySelector("#skills-list");
+  if (!skillsList) return;
+
+  skills.forEach((skill) => {
+    let listItem = document.createElement("li");
+    listItem.textContent = skill;
+    skillsList.appendChild(listItem);
+  });
+}
+
+// Function to render blogs
+function renderBlogs(blogs) {
+  let blogsList = document.querySelector("#blog-list");
+  if (!blogsList) return;
+
+  blogs.forEach((blog) => {
+    let blogItem = document.createElement("li");
+    blogItem.innerHTML = `<a href="${blog.blogurl}">${blog.blogname}</a>`;
+    blogsList.appendChild(blogItem);
+  });
+}
